398fd6a723e8eff9ff4405b72296bdd7a941682a
Juliantomaga <juliantorizky01@gmail.com>
2025-02-11 03:43:17 +0700

    make testimoni section

--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
-
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
     <link rel="icon" href="/images/logo-kolabora.png">
+    <!-- Swiper -->
+     <link rel="stylesheet" href="{{asset('library/swiper/swiper-bundle.min.css')}}">
     <title>Kolabora</title>
     @vite('resources/css/app.css')
 </head>
@@ -17,9 +18,39 @@
     @include('mainpage.about')
     @include('mainpage.goals')
     @include('mainpage.how-works')
+    @include('mainpage.testimoni')
 
 
     <!-- Icons -->
     <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
+
+    <!-- Swiper -->
+    <script src="{{asset('library/swiper/swiper-bundle.min.js')}}"></script>
+    <script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1, // Default untuk layar kecil
+        spaceBetween: 20,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2, // Untuk tablet
+            },
+            1024: {
+                slidesPerView: 3, // Untuk layar besar (menampilkan 3 card per slide)
+            }
+        }
+    });
+    </script>
 </body>
 </html>
---
+++ resources/views/mainpage/testimoni.blade.php
@@ -0,0 +1,159 @@
+<section class="testimoni w-full px-14 mx-auto mb-32" id="testimoni">
+    <div class="testimoni-container flex flex-col gap-y-4 w-full items-center">
+            <div class="testimoni-tagline">
+                <h3 class="mt-5 font-semibold text-lg bg-gray-100 rounded-full py-3 px-8 shadow-md">🌟 Apa Kata Mereka Tentang Kolabora</h3>
+            </div>
+            <div class="testimoni-description max-w-[70%]">
+            <p class="text-center font-light text-2xl">Kolabora telah membantu ribuan profesional dan tim untuk menemukan peluang kerja dan berkolaborasi lebih efektif. Ini pengalaman mereka!</p>
+        </div>
+    
+        <div class="relative testimoni-container-section w-full flex  swiper mySwiper items-center py-4">
+            <div class="testimoni-card-container-top flex items-center w-full rounded-xl swiper-wrapper mb-10">
+                <div class="testimoni-card flex flex-col  gap-y-3 bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20  p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Kolabora sangat memudahkan saya menemukan tim yang cocok untuk proyek freelance saya. Saya bisa langsung melihat skill mereka, pengalaman kerja, dan rating dari proyek sebelumnya.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Rizky Julianto</h3>
+                    </div>
+                </div>
+                <div class="testimoni-card flex flex-col  gap-y-3  bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20    p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Salah satu hal yang saya suka dari Kolabora adalah transparansi dalam setiap proyek. Saya bisa melihat detail pekerjaan, tenggat waktu, dan sistem pembayaran sebelum memutuskan untuk bergabung.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Igo Tegar</h3>
+                    </div>
+                </div>
+                <div class="testimoni-card flex flex-col  gap-y-3  bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20    p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Saya tidak menyangka bisa menemukan banyak proyek menarik di Kolabora. Ada banyak pilihan pekerjaan, mulai dari proyek kecil hingga yang berdurasi panjang, sehingga saya bisa menyesuaikannya dengan jadwal saya.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Faiz Ramadhani</h3>
+                    </div>
+                </div>
+
+                <div class="testimoni-card flex flex-col  gap-y-3  bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20    p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Berkat Kolabora, saya bisa membangun portofolio yang lebih baik. Setiap proyek yang saya selesaikan bisa saya tunjukkan ke klien lain, dan itu membantu saya mendapatkan lebih banyak pekerjaan.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Putri Cantika</h3>
+                    </div>
+                </div>
+                <div class="testimoni-card flex flex-col  gap-y-3  bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20    p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Saya sangat suka sistem Skill-to-Earn di Kolabora! Tidak hanya mendapatkan bayaran, saya juga bisa mengumpulkan poin dan menukarkannya dengan berbagai reward menarik.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Putra Perkasa</h3>
+                    </div>
+                </div>
+                <div class="testimoni-card flex flex-col  gap-y-3  bg-white border border-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20    p-4 rounded-md shadow-md mt-12 swiper-slide">
+                    <div class="testimoni-card-rates w-full flex justify-between items-center">
+                        <div class="testimoni-card-icon">
+                            <ul class="flex gap-x-2 text-yellow-300">
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                                <li><i class="ph ph-star"></i></li>
+                            </ul>
+                        </div>
+                        <div class="testimoni-card-quotes">
+                            <img src="{{asset('images/quotation.png')}}" alt="" class="w-10 h-8 object-contain">
+                        </div>
+                    </div>
+                    <div class="testimoni-card-description">
+                        <p class="text-justify text-md hyphens-manual">Saya awalnya kesulitan mencari pekerjaan freelance yang sesuai, tetapi setelah menggunakan Kolabora, saya dengan mudah menemukan proyek yang cocok dengan keahlian saya.</p>
+                    </div>
+                    <div class="testimoni-card-underline w-full h-1 bg-gray-100"></div>
+                    <div class="testimoni-card-name">  
+                        <h3 class="font-bold text-xl">Saifulrohim</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Donut shape -->
+             <img src="{{asset('images/donut_3d_shape.png')}}" alt="" class="absolute">
+             <img src="{{asset('images/donut_3d_shape.png')}}" alt="" class="absolute right-0 bottom-0">
+        <div class="swiper-pagination"></div>
+    </div>
+
+    </div>
+</section>
